f7161affa0d923c7bfcd4757d49a4275743a8792
wadudrahman <wadud@portwest.com>
2024-10-10 17:40:23 +0600

    UI Template Setup and Outlining Done

---
+++ resources/views/partials/table.blade.php
@@ -0,0 +1,64 @@
+<div class="d-flex border-bottom pb-3 flex-row-reverse">
+    <div class="my-2 my-md-0 flex-grow-1 flex-md-grow-0 order-first order-md-last">
+        <form action="./" method="get" autocomplete="off" novalidate="">
+            <div class="input-icon">
+                    <span class="input-icon-addon">
+                      <!-- Download SVG icon from http://tabler-icons.io/i/search -->
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                           stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                          <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+                          <path d="M21 21l-6 -6"></path>
+                      </svg>
+                    </span>
+                <input type="text" value="" class="form-control" placeholder="To Search Type & Press Enter">
+            </div>
+        </form>
+    </div>
+</div>
+<div class="table-responsive">
+    <table class="table card-table table-vcenter text-nowrap datatable">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Status</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>EVNT153248679</td>
+            <td>Introduction To Agile</td>
+            <td>10 Oct, 2024</td>
+            <td>09:00 AM - 05:00 PM</td>
+            <td><span class="badge bg-success me-1"></span>Upcoming</td>
+            <td class="text-end">
+                <span class="dropdown">
+                    <button class="btn dropdown-toggle align-text-top" data-bs-boundary="viewport"
+                            data-bs-toggle="dropdown">
+                        Actions
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-end">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                    </div>
+                </span>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+<div class="card-footer d-flex align-items-center">
+    <p class="m-0 text-muted">Showing <span>1</span> to <span>8</span> of
+        <span>16</span> entries</p>
+    <ul class="pagination m-0 ms-auto">
+        <li class="page-item active"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item"><a class="page-link" href="#">4</a></li>
+        <li class="page-item"><a class="page-link" href="#">5</a></li>
+    </ul>
+</div>
